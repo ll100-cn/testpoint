b1ec479ded126242de907a42f976cc933be32d60
Wangzongcan <wang.zongcan@gmail.com>
2017-07-10 16:10:34 +0800

    创建登录页面

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
+  before_action :authenticate_user!
+
   def root
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :registerable, :recoverable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 end
