caa06694bca829de60f0fc4ccf1b700608538564
Wangzongcan <wang.zongcan@gmail.com>
2017-07-13 15:41:13 +0800

    创建 plan

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712063022) do
+ActiveRecord::Schema.define(version: 20170713071613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "plans", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "test_cases", force: :cascade do |t|
     t.string "title"
