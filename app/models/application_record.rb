e03bc7152a9d5ec9279644898e6485e4f9c085a4
Wangzongcan <wang.zongcan@gmail.com>
2017-08-31 09:25:26 +0800

    将 test_cases 列表页面修改为 card

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,4 +3,16 @@ class ApplicationRecord < ActiveRecord::Base
 
   scope :where_exists, ->(scope) { where("EXISTS (?)", scope.select("1")) }
   scope :where_not_exists, ->(scope) { where.not("EXISTS (?)", scope.select("1")) }
+
+  def self.cleanup_column(*args)
+    options = { strip: true, presence: true }.merge(args.extract_options!)
+    args.each do |name|
+      before_validation if: -> { self.send("#{name}_changed?") } do
+        value = self.send(name)
+        value = value.try(:strip) if options[:strip]
+        value = value.try(:presence) if options[:presence]
+        self.send("#{name}=", value)
+      end
+    end
+  end
 end
