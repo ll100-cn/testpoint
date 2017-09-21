93d9752127e2c8b3ecba9c9fb84503b8c74b1098
Wangzongcan <wang.zongcan@gmail.com>
2017-09-21 13:57:01 +0800

    Case 页面使用 modal 编辑和新建

---
+++ app/javascript/widgets/modal.jsx
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+
+$(document).on('show.bs.modal', function(event) {
+  const $trigger = $(event.relatedTarget);
+  const url = $trigger.data('url');
+  if (url) {
+    const $modal = $(event.target);
+    $modal.find('.modal-content').load(url, function() {
+      $modal.trigger('content:loading');
+      $modal.trigger('content:loaded');
+    });
+  }
+});
