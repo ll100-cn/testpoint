adc62ddefc8cb2f50e6c0df8098821e9b5943e55
Wangzongcan <wang.zongcan@gmail.com>
2017-11-10 10:33:30 +0800

    添加 issue_id 到 tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,6 +9,7 @@
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #  platform_id  :integer
+#  issue_id     :integer
 #
 
 class Task < ApplicationRecord
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110021734) do
+ActiveRecord::Schema.define(version: 20171110022830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 20171110021734) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "platform_id"
+    t.bigint "issue_id"
+    t.index ["issue_id"], name: "index_tasks_on_issue_id"
     t.index ["plan_id"], name: "index_tasks_on_plan_id"
     t.index ["platform_id"], name: "index_tasks_on_platform_id"
     t.index ["test_case_id"], name: "index_tasks_on_test_case_id"
