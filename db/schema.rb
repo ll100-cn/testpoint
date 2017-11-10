69314f6063e6a5ace06630ca6ebd54ca8f2cc4f3
Wangzongcan <wang.zongcan@gmail.com>
2017-11-10 10:27:47 +0800

    添加 issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018055910) do
+ActiveRecord::Schema.define(version: 20171110021734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20171018055910) do
     t.datetime "updated_at", null: false
     t.string "ancestry"
     t.index ["ancestry"], name: "index_components_on_ancestry"
+  end
+
+  create_table "issues", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "plans", force: :cascade do |t|
---
+++ spec/factories/issues.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: issues
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  content    :text
+#  state      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+FactoryGirl.define do
+  factory :issue do
+    
+  end
+end
