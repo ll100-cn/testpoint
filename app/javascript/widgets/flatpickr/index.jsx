32200dbaec7a9264b9f243b905711f32739c12c5
Lucia <learningleadtorebirth@gmail.com>
2018-05-15 14:39:21 +0800

    refactoring selectTodayPlugin in jQuery style

--- app/javascript/widgets/flatpickr/index.jsx
+++ app/javascript/widgets/flatpickr/index.jsx
@@ -5,9 +5,6 @@ import 'flatpickr/dist/flatpickr.css'
 import 'flatpickr/dist/plugins/confirmDate/confirmDate.css'
 import 'flatpickr/dist/l10n'
 import 'widgets/flatpickr/style'
-// import { English } from 'flatpickr/dist/l10n/default'
-// import { Thailand }  from 'flatpickr/dist/l10n/th'
-import { Russian }  from 'flatpickr/dist/l10n/ru'
 
 
 var defaultConfig = {
@@ -18,48 +15,55 @@ var defaultConfig = {
 
 function SelectTodayPlugin (pluginConfig) {
     var config = Object.assign({}, defaultConfig, pluginConfig)
-    var btnContainer
-    var btnToday
-    var btnClear
     return function (fp) {
         if (fp.config.noCalendar || fp.isMobile) return {}
         return {
             onReady: function onReady () {
 
-                console.log(fp.config)
-
-                btnContainer = fp._createElement('div', 'flatpickr-btn-container ' + config.theme + 'Theme')
-                btnClear = fp._createElement('div', 'flatpickr-btn', config.clearText)
-                btnToday = fp._createElement('div', 'flatpickr-btn', config.todayText)
-
-                btnContainer.appendChild(btnClear)
-                btnContainer.appendChild(btnToday)
-                fp.calendarContainer.appendChild(btnContainer)
-
-
-                btnToday.addEventListener('click', function () {
-                    fp.setDate(fp.now)
+                var $btnContainer = $("<div>", {
+                    "class": "flatpickr-btn-container " + config.theme + 'Theme',
                 })
 
-                btnClear.addEventListener('click', function () {
+                var $btnClear = $("<div>", {
+                    "class": "flatpickr-btn",
+                    "text": config.clearText
+                })
+
+                var $btnToday = $("<div>", {
+                    "class": "flatpickr-btn",
+                    "text": config.todayText
+                })
+
+                $btnContainer.appendTo(fp.calendarContainer)
+                $btnClear.appendTo($btnContainer)
+                $btnToday.appendTo($btnContainer)
+
+                $btnClear.click(function () {
                     fp.input.value = ''
                     fp.close()
                 })
 
-                var plugins = fp.config.plugins
-                if (plugins.toString().indexOf('flatpickr-confirm')) {
-                    var nodes = fp.calendarContainer.childNodes
-                    for ( var i = 0; i < nodes.length ; i++) {
-                        if (nodes[i].className === 'flatpickr-confirm  lightTheme') {
-                            var btnConfirm = nodes[i]
-                            btnConfirm.classList.add('flatpickr-btn')
-                            btnContainer.appendChild(btnConfirm)
+                $btnToday.click(function () {
+                    fp.setDate(fp.now)
+                })
+
+                var $plugins = fp.config.plugins
+                if ($plugins.toString().includes('flatpickr-confirm')) {
+                    var $nodes = fp.calendarContainer.childNodes
+                    var addConfirm = false
+                    $nodes.forEach(function (node) {
+                        if ($.inArray("flatpickr-confirm", node.classList) > -1 ) {
+                            var $btnConfirm = $(node)
+                            $btnConfirm.addClass('flatpickr-btn')
+                            $btnConfirm.appendTo($btnContainer)
+                            addConfirm = true
                         }
-                    }
-                    if ( btnContainer.children.length === 2 ) {
+                    })
+                    if ( !addConfirm ) {
                         alert("confirmDatePlugin should be put after this plugin")
                     }
                 }
+
             }
         }
     }
@@ -69,8 +73,9 @@ $(document).on('turbolinks:load', function() {
     $(".timepicker").each(function() {
         $(this).flatpickr({
             enableTime: true,
-            locale: Russian,
+            locale: flatpickr.l10ns.zh,
             plugins: [ new SelectTodayPlugin(), new confirmDatePlugin() ]
         })
     })
+
 })
