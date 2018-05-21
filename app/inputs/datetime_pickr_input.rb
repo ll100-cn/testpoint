c287a8c3e742f61ee124a39d964d8c84b82e5335
Lucia <learningleadtorebirth@gmail.com>
2018-05-21 11:32:13 +0800

    rename method append to calendar_icon

--- app/inputs/datetime_pickr_input.rb
+++ app/inputs/datetime_pickr_input.rb
@@ -3,25 +3,20 @@ class DatetimePickrInput < SimpleForm::Inputs::Base
   def input
     template.content_tag(:div, class: 'input-group datetime-picker', data: { locale: I18n.locale.to_s.split("-").first }) do
       template.concat @builder.text_field(attribute_name, input_html_options)
-      template.concat append
+      template.concat calendar_icon
     end
   end
 
   def input_html_options
-    # super.deep_merge(data: { input: true }, style: 'border: 1px solid #c3c3c3;border-radius: 3px;')
     super.deep_merge(data: { input: true }, class: 'form-control')
   end
 
-  def append
-    template.content_tag(:div, class: 'input-group-append') do
-      template.concat calendar
-    end
-  end
-
-  def calendar
-    template.content_tag(:a, class: 'input-group-text', data: { toggle: true }) do
-      template.concat icon_calendar
-    end
+  def calendar_icon
+    template.content_tag(:div, class: 'input-group-append') {
+      template.content_tag(:a, class: 'input-group-text', data: { toggle: true }) {
+        template.concat icon_calendar
+      }
+    }
   end
 
   def icon_calendar
