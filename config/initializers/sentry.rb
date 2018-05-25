f3eced536960471eaf1e8b55fe969ec02b64f37d
Lucia <learningleadtorebirth@gmail.com>
2018-05-25 19:51:43 +0800

    set enviroment to production in sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,5 @@
 Raven.configure do |config|
   config.dsn = ENV['SENTRY_DNS']
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.environments = %w[production staging]
-  config.current_environment = "staging" if Rails.env.production? && Project.staging_environment?
+  config.environments = %w[production]
 end
