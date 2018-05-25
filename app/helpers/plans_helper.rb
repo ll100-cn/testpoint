5427ab318b09f1ba9671fa492f233d0d6f8bb6f6
Lucia <learningleadtorebirth@gmail.com>
2018-05-25 09:35:01 +0800

    refactor helper method task_state_with_icon

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -1,6 +1,6 @@
 module PlansHelper
-  def task_state_with_icon(text)
-    text_sym = text.to_sym
+  def task_state_with_icon(state)
+    text_sym = state.to_sym
     icons = { pending: "fa fa-circle-o", pass: "fa fa-check", failure: "fa fa-times" }
     class_names = { pending: "text-muted", pass: "text-success", failure: "text-danger" }
 
@@ -8,7 +8,7 @@ module PlansHelper
 
     content_tag :span, class: class_name do
       safe_join([
-        t(text, scope: "task.state".to_sym).capitalize,
+        state.text,
         content_tag(:span, "", class: icons[text_sym])
       ], ' ')
     end
