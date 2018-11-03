c3b7ee0157cdf126850f234e0b7a50069f4ca19b
lucia <learningleadtorebirth@gmail.com>
2018-11-03 14:52:49 +0800

    修复 attachment

--- db/migrate/20181102070913_fixed_attachment_content_type.rb
+++ db/migrate/20181102070913_fixed_attachment_content_type.rb
@@ -2,10 +2,14 @@ class FixedAttachmentContentType < ActiveRecord::Migration[5.2]
   class Attachment < ActiveRecord::Base; end
 
   def change
+    remove_column :attachments, :file, :string
+
     Attachment.where(content_type: nil).find_each do |attachment|
-      title = attachment.file.blob.filename
-      content_type = attachment.file.blob.content_type
-      attachment.update(content_type: content_type, title: title)
+      storage_attachment = ActiveStorage::Attachment.where(record_id: attachment.id, record_type: "Attachment").take!
+      storage_blob = ActiveStorage::Blob.where(id: storage_attachment.blob_id).take!
+      attachment.title = storage_blob.filename
+      attachment.content_type = storage_blob.content_type
+      attachment.save!
     end
   end
 end
