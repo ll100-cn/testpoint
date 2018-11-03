0cf5fbc853604848eb0079e386552ae436280dbe
lucia <learningleadtorebirth@gmail.com>
2018-11-03 14:42:56 +0800

    修复 attachment

---
+++ db/migrate/20181103063329_remove_attachment_file.rb
@@ -0,0 +1,15 @@
+class RemoveAttachmentFile < ActiveRecord::Migration[5.2]
+  class Attachment < ActiveRecord::Base; end
+
+  def change
+    remove_column :attachments, :file, :string
+
+    Attachment.where(content_type: nil).find_each do |attachment|
+      storage_attachment = ActiveStorage::Attachment.where(record_id: attachment.id, record_type: "Attachment").take!
+      storage_blob = ActiveStorage::Blob.where(id: storage_attachment.blob_id).take!
+      attachment.title = storage_blob.filename
+      attachment.content_type = storage_blob.content_type
+      attachment.save!
+    end
+  end
+end
