d735ab07116401d3fe4bc0d15a5c70596a1ee9a2
lucia <learningleadtorebirth@gmail.com>
2018-12-08 06:47:28 +0800

    fixed comments collapse

--- db/migrate/20181116082112_add_collapsed_to_comments.rb
+++ db/migrate/20181116082112_add_collapsed_to_comments.rb
@@ -1,4 +1,4 @@
-class AddHiddenToComments < ActiveRecord::Migration[5.2]
+class AddCollapsedToComments < ActiveRecord::Migration[5.2]
   def change
     add_column :comments, :collapsed, :boolean, default: false
   end
