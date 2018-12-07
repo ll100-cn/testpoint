a254f6550dca66da6c33b8e17864f6cf3818e060
lucia <learningleadtorebirth@gmail.com>
2018-12-07 11:31:28 +0800

    statistic state filter quantity

--- db/migrate/20181102070913_fixed_attachment_content_type.rb
+++ db/migrate/20181102070913_fixed_attachment_content_type.rb
@@ -2,8 +2,6 @@ class FixedAttachmentContentType < ActiveRecord::Migration[5.2]
   class Attachment < ActiveRecord::Base; end
 
   def change
-    remove_column :attachments, :file, :string
-
     Attachment.where(content_type: nil).find_each do |attachment|
       storage_attachment = ActiveStorage::Attachment.where(record_id: attachment.id, record_type: "Attachment").take!
       storage_blob = ActiveStorage::Blob.where(id: storage_attachment.blob_id).take!
@@ -11,5 +9,7 @@ class FixedAttachmentContentType < ActiveRecord::Migration[5.2]
       attachment.content_type = storage_blob.content_type
       attachment.save!
     end
+
+    remove_column :attachments, :file, :string
   end
 end
