9bfbcc255c59ce374306a59aceec2d30ecdd0452
lupeipei <learningleadtorebirth@gmail.com>
2019-10-17 15:34:16 +0800

    修复 fancybox 关闭后 overlay 未移除问题

--- app/javascript/components/fancybox.jsx
+++ app/javascript/components/fancybox.jsx
@@ -1,2 +1,7 @@
 import '@fancyapps/fancybox/dist/jquery.fancybox.css'
 import '@fancyapps/fancybox/dist/jquery.fancybox.js'
+
+$(document).on('beforeClose.fb', (_e, _instance, _slide) => {
+  $(".fancybox-container").remove();
+  return true;
+});
