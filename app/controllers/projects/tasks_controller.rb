c0f7f900f5861a27a600aced45c7edc1e5ddd4c4
vincentying15 <vincent_ying_live@outlook.com>
2019-12-13 15:12:56 +0800

    Update plans#show, current_folder wont change after finishing task

--- app/controllers/projects/tasks_controller.rb
+++ app/controllers/projects/tasks_controller.rb
@@ -5,12 +5,15 @@ class Projects::TasksController < BaseProjectController
   load_and_authorize_resource through: :plan
   load_and_authorize_resource through: :project
 
+  def row
+  end
+
   def edit
   end
 
   def update
     @task.update(task_params)
-    respond_with @task, location: ok_url_or_default([@project, @task.plan])
+    @related_issues = @project.issues.left_outer_joins(:tasks).where(tasks: { test_case_id: @task.test_case.id }).records
   end
 
   def change_state
