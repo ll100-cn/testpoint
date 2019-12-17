15cd8bda49abeee23279ea5b260de22ae3bc7924
vincentying15 <vincent_ying_live@outlook.com>
2019-12-17 14:21:13 +0800

    Fix test_case tree folder count

--- app/controllers/projects/test_cases_controller.rb
+++ app/controllers/projects/test_cases_controller.rb
@@ -9,14 +9,14 @@ class Projects::TestCasesController < BaseProjectController
 
   def index
     @default_cases_url_options = request.query_parameters
-    @test_cases = @test_cases.available
-    @test_cases = @test_cases.where_exists(Platform.connect_test_cases.where(id: @platform)) if @platform
+    test_cases_scope = @test_cases.available
+    test_cases_scope = test_cases_scope.where_exists(Platform.connect_test_cases.where(id: @platform)) if @platform
     @test_cases = @test_cases.where(folder_id: @folder.subtree) if @folder
     @test_cases = @test_cases.page(params[:page])
 
     @folders = @project.folders.available.ranked
 
-    test_cases_counts = @test_cases.group(:folder_id).count
+    test_cases_counts = test_cases_scope.group(:folder_id).count
     @folder_test_cases_counts = Folder.descendants_with_self_counts(@folders, test_cases_counts)
   end
 
@@ -25,10 +25,7 @@ class Projects::TestCasesController < BaseProjectController
 
   def create
     @test_case.save
-    respond_with @test_case, location: ok_url_or_default([@project, TestCase])
-  end
-
-  def show
+    respond_with @test_case, location: -> { ok_url_or_default([@project, TestCase]) }
   end
 
   def edit
@@ -36,12 +33,12 @@ class Projects::TestCasesController < BaseProjectController
 
   def update
     @test_case.update(test_case_params)
-    respond_with @test_case, action: :edit, location: ok_url_or_default([@project, TestCase])
+    respond_with @test_case
   end
 
   def destroy
     @test_case.archive
-    respond_with @test_case, location: ok_url_or_default([@project, TestCase])
+    respond_with @test_case, location: -> { ok_url_or_default([@project, TestCase]) }
   end
 
 protected
