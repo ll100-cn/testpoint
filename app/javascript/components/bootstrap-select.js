38639336ff31ae31a1d035c1a356ef7232ddaafd
vincentying15 <vincent_ying_live@outlook.com>
2021-09-23 17:37:33 +0800

    修复 turbolinks 返回页面错误

--- app/javascript/components/bootstrap-select.js
+++ app/javascript/components/bootstrap-select.js
@@ -3,6 +3,9 @@ import 'bootstrap-select'
 import 'bootstrap-select/dist/css/bootstrap-select.css'
 
 $(document).on('content:loaded', function(event) {
-  console.log(event.target)
   $(event.target).find('.selectpicker').selectpicker()
+})
+
+$(document).on('turbolinks:before-cache', function(event) {
+  $('.selectpicker').selectpicker('destroy').addClass("selectpicker")
 })
