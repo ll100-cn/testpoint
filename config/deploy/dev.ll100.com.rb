5800bc92e18a79088635c9e913675fd6fabe329b
Guoxweii <alphaguoxiongwei@gmail.com>
2023-08-09 11:22:20 +0800

    修复链接错误

--- config/deploy/dev.ll100.com.rb
+++ config/deploy/dev.ll100.com.rb
@@ -1,3 +1,3 @@
 # server 'app@dev.ll100.com:5011', roles: %w[web app db], primary: true , sidekiq: true, whenever: true
-set :default_env, { RAILS_RELATIVE_URL_ROOT: "/testpoint", SHAKAPACKER_ASSET_HOST: "/testpoint/" }
+set :default_env, { RAILS_RELATIVE_URL_ROOT: "/testpoint/", SHAKAPACKER_ASSET_HOST: "/testpoint/" }
 server 'app@dev.ll100.com:5011', roles: %w[web app db], primary: true
