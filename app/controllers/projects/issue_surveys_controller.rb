d3507d0f995c8c0cf14f9451b9ba64f921e12172
Guoxweii <alphaguoxiongwei@gmail.com>
2023-09-21 10:36:07 +0800

    修复错误的文件名和类名

--- app/controllers/projects/issue_surveys_controller.rb
+++ app/controllers/projects/issue_surveys_controller.rb
@@ -1,4 +1,4 @@
-class Projects::IssueSurveiesController < BaseProjectController
+class Projects::IssueSurveysController < BaseProjectController
   load_and_authorize_resource :project
   load_and_authorize_resource :issue
   load_and_authorize_resource through: :issue
