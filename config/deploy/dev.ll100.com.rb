6204e5a9033a7ef9c2a234edd586f4cf30977eed
Guoxweii <alphaguoxiongwei@gmail.com>
2023-09-21 10:19:08 +0800

    设置发布时内存占用量为 1 g

--- config/deploy/dev.ll100.com.rb
+++ config/deploy/dev.ll100.com.rb
@@ -1,3 +1,5 @@
 # server 'app@dev.ll100.com:5011', roles: %w[web app db], primary: true , sidekiq: true, whenever: true
 set :default_env, { RAILS_RELATIVE_URL_ROOT: "/testpoint/", VITE_RUBY_BASE: "/testpoint/" }
 server 'app@dev.ll100.com:5011', roles: %w[web app db], primary: true
+
+fetch(:remote_env)["NODE_OPTIONS"] = "--max-old-space-size=1024"
