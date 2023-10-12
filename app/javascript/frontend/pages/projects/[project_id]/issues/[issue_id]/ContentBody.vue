23a8d5a089c64cd066ea234056752c5afcb58eea
Guoxweii <alphaguoxiongwei@gmail.com>
2023-10-12 14:01:16 +0800

    附件布局页面

--- app/javascript/frontend/pages/projects/[project_id]/issues/[issue_id]/ContentBody.vue
+++ app/javascript/frontend/pages/projects/[project_id]/issues/[issue_id]/ContentBody.vue
@@ -5,7 +5,7 @@
       最后修改于: {{ h.datetime(body.last_edited_at) }}
     </span>
 
-    <div class="row mt-2" v-if="body.attachments.length > 0">
+    <div class="row mt-2 gy-2" v-if="body.attachments.length > 0">
       <div v-for="attachment in body.attachments" :key="attachment.id" class="col-lg-6">
         <AttachmentInfo :editable="editable" :attachment="attachment" @deleted="emit('attachment_destroyed', $event)" @edited="emit('attachment_updated', $event)" />
       </div>
