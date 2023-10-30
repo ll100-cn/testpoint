bfc1cb84b1b8424a09bc6881aa13b39da0fce831
Guoxweii <alphaguoxiongwei@gmail.com>
2023-10-30 13:14:31 +0800

    更多按钮如果没有功能不需要显示

--- app/javascript/frontend/components/MoreDropdown.vue
+++ app/javascript/frontend/components/MoreDropdown.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="dropdown dropdown-no-arrow">
+  <div class="dropdown dropdown-no-arrow" v-show="shown">
     <button class="btn btn-sm dropdown-toggle" data-bs-toggle="dropdown">
       <i class="far fa-ellipsis-h"></i>
     </button>
-    <div class="dropdown-menu dropdown-menu-end">
+    <div ref="content_node" class="dropdown-menu dropdown-menu-end">
       <slot />
     </div>
   </div>
 </template>
+<script setup lang="ts">
+import _ from 'lodash'
+import { onMounted, ref } from 'vue'
+
+const content_node = ref(null! as HTMLDivElement)
+const shown = ref(false)
+
+onMounted(() => {
+  shown.value = !_.isEmpty(content_node.value.innerHTML)
+})
+</script>
