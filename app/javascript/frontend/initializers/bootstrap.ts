5eac345ecc06dc0176ed8c3a868f33020190ec32
Guoxweii <alphaguoxiongwei@gmail.com>
2024-04-30 11:42:30 +0800

    修复 modal 的 route-link 跳转后返回 页面无法滚动的问题

--- app/javascript/frontend/initializers/bootstrap.ts
+++ app/javascript/frontend/initializers/bootstrap.ts
@@ -1,5 +1,6 @@
-import { AppContext } from "@/types";
-import { Tooltip } from 'bootstrap'
+import { AppContext } from "@/types"
+import { Modal, Tooltip } from 'bootstrap'
+import _ from "lodash"
 
 export default function({ app }: AppContext) {
   app.directive('bs-tooltip', function(el, binding) {
@@ -8,5 +9,21 @@ export default function({ app }: AppContext) {
       placement: binding.arg as any,
       trigger: 'hover'
     })
+  })
+
+  const router = app.config.globalProperties.$router
+  router.beforeEach((to, from, next) => {
+    const elements = document.querySelectorAll('.modal.show')
+    const modals = _(elements).map(it => Modal.getInstance(it)).compact().value()
+
+    if (modals.length > 0) {
+      document.addEventListener('hidden.bs.modal', () => next(), { once: true })
+
+      for (const modal of modals) {
+        modal.hide()
+      }
+    } else {
+      next()
+    }
   })
 }
