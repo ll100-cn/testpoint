96bf5f2755a2de2084476b658d18af8666d6cde3
Guoxweii <alphaguoxiongwei@gmail.com>
2025-03-10 11:17:40 +0800

    支持黑色模式

--- app/javascript/frontend/components/controls/presets.ts
+++ app/javascript/frontend/components/controls/presets.ts
@@ -2,7 +2,7 @@ import { bva } from "$ui/utils"
 
 export const standard = {
   input: bva(`
-    block w-full border border-input-border rounded text-foreground bg-white
+    block w-full border border-input-border rounded text-foreground bg-input-background
     focus:border-primary/50 focus:ring-4 focus:ring-primary/25
     disabled:bg-muted/10
   `, {
