c4b340bb6706367db59b24d592a65a88a92b458b
Guoxweii <alphaguoxiongwei@gmail.com>
2025-06-18 12:58:58 +0800

    修复权限问题

--- app/controllers/api/v2/projects/issues_controller.rb
+++ app/controllers/api/v2/projects/issues_controller.rb
@@ -1,7 +1,7 @@
 class Api::V2::Projects::IssuesController < Api::V2::Projects::BaseController
   before_action -> { @project = current_project }
   load_and_authorize_resource through: :project, authorization_action: ->(action) {
-    { archive: :create, unresolve: :create, body: :update, merge: :manage, summary: :index }[action]
+    { merge: :manage, process2: :manage, resolve: :manage, summary: :index }[action]
   }
 
   def index
